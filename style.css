861a6832324f2bfb35a68224798802a3d8474a2c
oscartzintzun <oscartzintzun123@gmail.com>
2017-04-01 19:30:09 -0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,164 @@
+Body {
+  width: 100%;
+  margin: auto;
+}
+
+.container {
+  width: 960px
+  margin: 0 auto;
+}
+
+.header {
+  background: #FF3366;
+  width: 100%;
+  top: 0;
+  position: fixed;
+}
+
+footer {
+    text-align: center;
+    bottom : 0;
+    height : 40px;
+    margin-top : 40px;
+  }
+
+.Logo {
+  float:left;
+  font-famliy: "Helvetica";
+    font-size: 15px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
+li{
+  list-style: none;
+  float: left;
+  margin-left: 15px;
+  padding-top: 5px;
+}
+
+.nav{
+  float: right;
+  margin-right:150px;
+  padding-top: 10px;
+}
+
+body {
+  color: #FF3366;
+}
+
+img {
+	width:100px;
+	height:100px;
+}
+table {
+    width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+}
+thead, tbody {
+    background-color: #FF3366;
+}
+th {
+    width: 100px;
+    height: 40px;
+    color: white;
+    text-align: center;
+    font-weight: bold;
+}
+td {
+    color: white;
+    list-style-type: none;
+    text-align: center;
+}
+legend {
+    font-family: Open Sans, sans-serif;
+    color: #FF3366;
+}
+label {
+    font-family: Georgia, serif;
+    color: #FF3366;
+    margin-left: 2%;
+}
+fieldset {
+    border: 1px solid #FF3366;
+}
+input[type="text"], input[type="number"] {
+    font-family: Georgia, serif;
+    color: #FF3366;
+}
+input[type="number"] {
+    width: 50px;
+}
+input[type="button"] {
+    background-color: #FF3366;
+    border: none;
+    font-family: Open Sans, sans-serif;
+    font-size: .8em;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 12px;
+    display: inline-block;
+    float: right;
+    margin-right: 50px;
+}
+input.required {
+    background-color: #B1522A;
+    color: #CAB467;
+}
+.main {
+    margin: 0;
+    padding: 0;
+    height: 1em;
+}
+.main li {
+    list-style: none;
+    float: right;
+    border-left: 1px solid #FF3366;
+    width: 7em;
+}
+.main li a:not(.active) {
+    display: block;
+    padding: 3px 8px;
+    background-color: #B1522A;
+    color: #CAB467;
+    text-decoration: none;
+}
+.active {
+    display: block;
+    padding: 3px 8px;
+    background-color: #CAB467;
+    color: #302C26;
+    text-decoration: none;
+}
+.main li ul {
+    display: none;
+    width: 10em;
+    background-color: #302C26;
+}
+.main li:hover ul {
+    display: block;
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    background-color: #302C26;
+}
+.main li:hover li {
+    float: none;
+}
+.main li:hover li a {
+    background-color: #B1522A;
+    border-bottom: 1px solid #CAB467;
+    color: #CAB467;
+}
+.main li li a:hover {
+    background-color: #302C26;
+}
+
+.small {
+  font-size: 0.8em;
+}
